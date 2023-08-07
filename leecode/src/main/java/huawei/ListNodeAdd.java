e03dd65c01cbf4e5a5dae97d55d9e7a7d7adf703
wangxiaofeng <1562200285@qq.com>
2023-08-07 20:05:19 +0800

    防范XSS攻击和SQL注入风险

---
+++ leecode/src/main/java/huawei/ListNodeAdd.java
@@ -0,0 +1,20 @@
+package huawei;
+
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2023-08-04 18:19
+ * @description：两个列表数字相加
+ */
+public class ListNodeAdd {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+
+        return result;
+    }
+    public class ListNode {
+        int val;
+        Solution.ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
--- singleton/src/main/java/com/bhfae/service/Test.java
+++
@@ -1,8 +0,0 @@
-package com.bhfae.service;
-
-import org.springframework.stereotype.Service;
-
-public interface Test {
-
-    public void test();
-}
