658a4cdeaea5584484349a9eb9b93bb2c972bd81
wangxiaofeng <1562200285@qq.com>
2023-08-09 18:42:57 +0800

    链表_删除链表中指定值

---
+++ leecode/src/main/java/代码随想录/数组/_59螺旋矩阵.java
@@ -0,0 +1,19 @@
+package 代码随想录.数组;
+
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2023-08-08 21:17
+ * @description：
+ */
+public class _59螺旋矩阵 {
+
+    public static int[][] generateMatrix(int n) {
+        int[][] result = new int[][]{};
+        int count = 0;
+        int loop = n%2;
+        for (int index = 0; index < n*n; index++) {
+
+        }
+        return result;
+    }
+}
---
+++ leecode/src/main/java/代码随想录/链表/_203移除链表元素.java
@@ -0,0 +1,42 @@
+package 代码随想录.链表;
+
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2023-08-09 16:53
+ * @description：
+ */
+public class _203移除链表元素 {
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        ListNode listNode = new ListNode(7);
+        node.next = listNode;
+
+        System.out.println(removeElements(node,7));
+    }
+
+
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return dummyHead.next;
+    }
+}
+
+
+ class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
